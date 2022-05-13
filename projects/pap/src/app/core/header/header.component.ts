b340e3141d61adf11b9b031e050ee53defacef17
Niccolò Biondi <niccolo@3logic.it>
2022-05-13 18:08:40 +0200

    app.pap.222.0.5: header component

--- projects/pap/src/app/core/header/header.component.ts
+++ projects/pap/src/app/core/header/header.component.ts
@@ -1,4 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { Store } from '@ngrx/store';
+import { of } from 'rxjs';
+import { headerInfo } from '../../models/headerInfo';
+import { AppState } from '../core.state';
+
+const headermock: headerInfo = {
+  label: 'portAPPorta',
+  // img: 'assets/icons/logo-e.png',
+  showBack: false,
+  buttonStart: {
+    label: '',    
+    action: 'open-menu',
+    icon:'menu'
+  },
+  buttonEnd: {
+    label: '',    
+    action: 'navigate',
+    url: 'settings',
+    icon:'settings'
+  },
+};
 
 @Component({
   selector: 'pap-header',
@@ -7,9 +29,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  headerView$ = of(headermock);
+  constructor(private _store: Store<AppState>, private _navCtrl: NavController) {
+
+  }
 
   ngOnInit(): void {
+  }
+
+  action(action: string, url?: string) {
+    console.log("------- ~ HeaderComponent ~ action ~ action, url", action, url);
   }
 
 }
--- projects/pap/src/app/models/headerInfo.ts
+++ projects/pap/src/app/models/headerInfo.ts
@@ -3,6 +3,7 @@ import { buttonInfo } from "./buttonInfo";
 export interface headerInfo {
     label?: string,
     img?: string,
+    showBack: boolean;
     buttonStart?: buttonInfo,
     buttonEnd?: buttonInfo,
 }
