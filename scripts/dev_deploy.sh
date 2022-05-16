c526d6af3f704f20876a6f344681d0b9a3287c18
bongiu <peppedeka@gmail.com>
2022-05-16 11:43:19 +0200

    build: add deploy script

---
+++ scripts/dev_deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+echo "Deployment started ..."
+
+# Pull the latest version of the app
+git pull origin develop
+
+# install libraries
+npm install
+
+# create build
+npm run build
+
+# delete old build
+ rm -rf /var/www/html/dev.app.portapporta.webmapp.it/*
+
+#move new build to www
+mv /pap/dist/pap/* /var/www/html/dev.app.portapporta.webmapp.it/ -f
+
+echo "Deployment finished!"
