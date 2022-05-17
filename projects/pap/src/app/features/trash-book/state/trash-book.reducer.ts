bd87cbc84b202f16d7e0aa1684d0e60eac0f3d46
bongiu <peppedeka@gmail.com>
2022-05-17 10:43:19 +0200

    app.pap.222.4.1: link state

--- projects/pap/src/app/features/trash-book/state/trash-book.reducer.ts
+++ projects/pap/src/app/features/trash-book/state/trash-book.reducer.ts
@@ -1,16 +1,27 @@
 import {Action, createReducer, on} from '@ngrx/store';
+import {TrashBookRow} from '../trash-book-model';
 import * as TrashBookActions from './trash-book.actions';
 
 export const trashBookFeatureKey = 'trashBook';
 
-export interface State {}
+export interface TrashBookState {
+  trashBook: TrashBookRow[];
+}
 
-export const initialState: State = {};
+export const initialState: TrashBookState = {
+  trashBook: [],
+};
 
 export const reducer = createReducer(
   initialState,
-
   on(TrashBookActions.loadTrashBooks, state => state),
   on(TrashBookActions.loadTrashBooksSuccess, (state, action) => state),
   on(TrashBookActions.loadTrashBooksFailure, (state, action) => state),
 );
+
+export function trashBookReducer(
+  state: TrashBookState | undefined,
+  action: Action,
+): TrashBookState {
+  return reducer(state, action);
+}
---
+++ projects/pap/src/app/features/trash-book/trash-book-model.ts
@@ -0,0 +1,10 @@
+export interface TrashBookRow {
+  id: number;
+  name: string;
+  notes: string;
+  pap: boolean;
+  delivery: boolean;
+  collection_center: boolean;
+  translations: any;
+  trash_type_id: number;
+}
