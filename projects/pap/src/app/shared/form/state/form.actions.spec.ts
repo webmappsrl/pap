b18d49b278494fdd3273b2d671f0235ffa560491
bongiu <peppedeka@gmail.com>
2022-05-30 07:55:15 +0200

    remove useless files: please do not it again

--- projects/pap/src/app/shared/form/state/form.actions.spec.ts
+++
@@ -1,7 +0,0 @@
-import * as fromForm from './form.actions';
-
-describe('loadForms', () => {
-  it('should return an action', () => {
-    expect(fromForm.loadForms().type).toBe('[Form] Load Forms');
-  });
-});
