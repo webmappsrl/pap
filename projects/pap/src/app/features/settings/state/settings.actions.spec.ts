ea69b469f5acbced4888fb48ee98d44fcd8a6cc3
Niccolò Biondi <niccolo@3logic.it>
2022-06-07 10:03:24 +0200

    app.pap.222.15.2: remove spec files

--- projects/pap/src/app/features/settings/state/settings.actions.spec.ts
+++
@@ -1,7 +0,0 @@
-import * as fromSettings from './settings.actions';
-
-describe('loadSettingss', () => {
-  it('should return an action', () => {
-    expect(fromSettings.loadSettingss().type).toBe('[Settings] Load Settingss');
-  });
-});
