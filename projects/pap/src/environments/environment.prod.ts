758514b78cfc6c40c825897139d0554ad50caf14
bongiu <peppedeka@gmail.com>
2022-06-10 15:50:47 +0200

    ...

--- projects/pap/src/environments/environment.prod.ts
+++ projects/pap/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api: 'http://dev.portapporta.webmapp.it',
+  api: 'http://portapporta.webmapp.it',
   //api: 'http://127.0.0.1:8000/',
   GOOOGLEAPIKEY: '',
 };
