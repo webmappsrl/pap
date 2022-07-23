40679c5bd63b944096cd900815f8c94f241f99c1
bongiu <peppedeka@gmail.com>
2022-07-23 10:09:55 +0200

    fix conf and layout

--- projects/pap/src/environments/environment.prod.ts
+++ projects/pap/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api: 'http://portapporta.webmapp.it',
+  api: 'https://portapporta.webmapp.it',
   //api: 'http://127.0.0.1:8000/',
   GOOOGLEAPIKEY: '',
 };
--- projects/pap/src/environments/environment.ts
+++ projects/pap/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   test: false,
   // api: 'http://dev.portapporta.webmapp.it',
-  api: 'http://portapporta.webmapp.it',
+  api: 'https://portapporta.webmapp.it',
   // api: 'http://127.0.0.1:8000',
   GOOOGLEAPIKEY: '',
 };
