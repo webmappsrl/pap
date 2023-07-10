57c46b11c26a61bcbe25acb2794a1ec2098d1371
bongiu <peppedeka@gmail.com>
2023-07-10 15:06:51 +0200

    feat(gulp): update

--- gulpfile.ts
+++ gulpfile.ts
@@ -5,71 +5,29 @@ import * as fsExtra from 'fs-extra';
 import axios from 'axios';
 import {exec} from 'child_process';
 import {CapacitorConfig} from '@capacitor/cli';
-const variables = `:root {
-	--ion-color-primary: #eb445a;
-	--ion-color-primary-rgb: 235,68,90;
-	--ion-color-primary-contrast: #ffffff;
-	--ion-color-primary-contrast-rgb: 255,255,255;
-	--ion-color-primary-shade: #cf3c4f;
-	--ion-color-primary-tint: #ed576b;
-
-	--ion-color-secondary: #5260ff;
-	--ion-color-secondary-rgb: 82,96,255;
-	--ion-color-secondary-contrast: #ffffff;
-	--ion-color-secondary-contrast-rgb: 255,255,255;
-	--ion-color-secondary-shade: #4854e0;
-	--ion-color-secondary-tint: #6370ff;
-
-	--ion-color-tertiary: #5260ff;
-	--ion-color-tertiary-rgb: 82,96,255;
-	--ion-color-tertiary-contrast: #ffffff;
-	--ion-color-tertiary-contrast-rgb: 255,255,255;
-	--ion-color-tertiary-shade: #4854e0;
-	--ion-color-tertiary-tint: #6370ff;
-
-	--ion-color-success: #2dd36f;
-	--ion-color-success-rgb: 45,211,111;
-	--ion-color-success-contrast: #000000;
-	--ion-color-success-contrast-rgb: 0,0,0;
-	--ion-color-success-shade: #28ba62;
-	--ion-color-success-tint: #42d77d;
-
-	--ion-color-warning: #ffc409;
-	--ion-color-warning-rgb: 255,196,9;
-	--ion-color-warning-contrast: #000000;
-	--ion-color-warning-contrast-rgb: 0,0,0;
-	--ion-color-warning-shade: #e0ac08;
-	--ion-color-warning-tint: #ffca22;
-
-	--ion-color-danger: #eb445a;
-	--ion-color-danger-rgb: 235,68,90;
-	--ion-color-danger-contrast: #ffffff;
-	--ion-color-danger-contrast-rgb: 255,255,255;
-	--ion-color-danger-shade: #cf3c4f;
-	--ion-color-danger-tint: #ed576b;
-
-	--ion-color-medium: #92949c;
-	--ion-color-medium-rgb: 146,148,156;
-	--ion-color-medium-contrast: #000000;
-	--ion-color-medium-contrast-rgb: 0,0,0;
-	--ion-color-medium-shade: #808289;
-	--ion-color-medium-tint: #9d9fa6;
-
-	--ion-color-light: #f4f5f8;
-	--ion-color-light-rgb: 244,245,248;
-	--ion-color-light-contrast: #000000;
-	--ion-color-light-contrast-rgb: 0,0,0;
-	--ion-color-light-shade: #d7d8da;
-	--ion-color-light-tint: #f5f6f9;
-
-}`;
+const api = 'https://dev.portapporta.webmapp.it';
 interface Config {
   id: number;
   name: string;
   resources: {
     icon: string;
     splash: string;
+    variables: string;
+    logo: string;
+    header_image: string;
+    footer_image: string;
   };
+  sku: string;
+}
+interface Paths {
+  apiUrl: string;
+  capacitorConfigPath: string;
+  companyId: string;
+  devVariablesConfigPath: string;
+  environmentDevPath: string;
+  environmentProdPath: string;
+  instancePath: string;
+  variablesConfigPath: string;
 }
 const jsonEditor = require('gulp-json-editor');
 // Carica le variabili d'ambiente dal file .env
@@ -78,25 +36,23 @@ dotenv.config();
 // Task per il build e il deploy dell'app
 gulp.task('build', async () => {
   try {
-    const appId = process.argv[4];
-    const apiUrl = `https://portapporta.webmapp.it/api/c/${appId}/config.json`;
-    const instancePath = `./instances/${appId}`;
-    const capacitorConfigPath = `${instancePath}/capacitor-config.json`;
-    const variablesConfigPath = `projects/pap/src/theme/variables.scss`;
-    const environmentProdPath = `projects/pap/src/environments/environment.prod.ts`;
-    const deploys = await readJsonFile('deploys.json');
-    const deploy = deploys[appId];
-    const oldVariables = await readFileContent(variablesConfigPath);
+    const companyId = process.argv[4];
+    const paths: Paths = getPaths(companyId);
+
+    const oldVariables = await readFileContent(paths.variablesConfigPath);
+
+    const devVariables = await readFileContent(paths.devVariablesConfigPath);
+    await execCmd(`rm -rf ${paths.devVariablesConfigPath}`);
     const oldCapacitorConfig = await readFileContent('capacitor.config.ts');
 
-    const config: Config = await getConfig(apiUrl);
+    const config: Config = await getConfig(paths.apiUrl);
     console.log('Configurazione ottenuta:', config);
-    const sku = deploy.sku;
+    const sku = config.sku;
     console.log('Sku:', sku);
 
-    createFolder(instancePath);
+    createFolder(paths.instancePath);
     const capacitorConfig: CapacitorConfig = {
-      appId: sku,
+      appId: config.sku,
       appName: config.name,
       webDir: 'dist/pap/',
       bundledWebRuntime: false,
@@ -114,45 +70,132 @@ gulp.task('build', async () => {
     };
     const environment = {
       production: true,
-      companyId: appId,
-      api: 'https://portapporta.webmapp.it',
-      //api: 'http://127.0.0.1:8000/',
+      companyId: companyId,
+      config,
+      api,
       GOOOGLEAPIKEY: '',
     };
 
-    await writeFile(capacitorConfigPath, JSON.stringify(capacitorConfig));
+    await writeFile(paths.capacitorConfigPath, JSON.stringify(capacitorConfig));
     await writeFile(
       'capacitor.config.ts',
-      `import {CapacitorConfig} from '@capacitor/cli';
-
+      `
+      import {CapacitorConfig} from '@capacitor/cli';
       const capacitorConfig: CapacitorConfig = ${JSON.stringify(capacitorConfig)}
-    
-    export default capacitorConfig;`,
+      export default capacitorConfig;
+      `,
     );
     await writeFile(
-      environmentProdPath,
-      `export const environment = ${JSON.stringify(environment)}`,
+      paths.environmentProdPath,
+      `export const environment = ${JSON.stringify(environment).replace(/"([^"]+)":/g, '$1:')}`,
     );
-    await writeFile(variablesConfigPath, variables);
-    await ionicBuild(capacitorConfigPath, instancePath);
-    await ionicBuildIos(capacitorConfigPath, instancePath);
+    await writeFile(paths.variablesConfigPath, config.resources.variables);
+    await writeFile(paths.devVariablesConfigPath, config.resources.variables);
+    await ionicBuild(paths.capacitorConfigPath, paths.instancePath);
+    await ionicBuildIos(paths.capacitorConfigPath, paths.instancePath);
     await ionicPlathforms(config.resources);
     await ionicCapSync();
 
-    await writeFile(variablesConfigPath, oldVariables);
+    await writeFile(paths.variablesConfigPath, oldVariables);
     await writeFile('capacitor.config.ts', oldCapacitorConfig);
-    await moveFoldersToInstance(instancePath);
+    await writeFile(paths.devVariablesConfigPath, devVariables);
+    await moveFoldersToInstance(paths.instancePath);
     console.log('Build completed successfully.');
   } catch (err) {
     console.error('An error occurred during the build:', err);
     throw err;
   }
 });
+
+gulp.task('serve', async () => {
+  const companyId = process.argv[4];
+  const paths: Paths = getPaths(companyId);
+  const config: Config = await getConfig(paths.apiUrl);
+
+  await setDevEnvironment(paths, config);
+  await writeFile(paths.devVariablesConfigPath, config.resources.variables);
+  await setAssets(config);
+  await execCmd(`ionic serve`);
+});
+
+gulp.task('init', async () => {
+  await execCmd(`rm -rf android`);
+  await execCmd(`rm -rf ios`);
+  await execCmd(`rm -rf resources`);
+  await execCmd(`rm -rf icons`);
+  await execCmd(`npx cap add ios`);
+  await execCmd(`cd ios/App && pod install`);
+  await execCmd(`npx cap add android`);
+});
 // Task predefinito
 gulp.task('default', gulp.series('build'));
+
+async function setDevEnvironment(paths: Paths, config: Config): Promise<void> {
+  const environment = {
+    production: true,
+    companyId: paths.companyId,
+    config,
+    api,
+    //api: 'http://127.0.0.1:8000/',
+    GOOOGLEAPIKEY: '',
+  };
+
+  await writeFile(
+    paths.environmentDevPath,
+    `export const environment = ${JSON.stringify(environment).replace(/"([^"]+)":/g, '$1:')}`,
+  );
+}
+async function setProdEnvironment(paths: Paths, config: Config): Promise<void> {
+  const environment = {
+    production: true,
+    companyId: paths.companyId,
+    config,
+    api,
+    //api: 'http://127.0.0.1:8000/',
+    GOOOGLEAPIKEY: '',
+  };
+
+  await writeFile(
+    paths.environmentProdPath,
+    `export const environment = ${JSON.stringify(environment).replace(/"([^"]+)":/g, '$1:')}`,
+  );
+}
+
+async function setAssets(config: Config): Promise<void> {
+  await download(config.resources.logo, `projects/pap/src/assets/icons/logo.png`);
+  await download(config.resources.header_image, `projects/pap/src/assets/images/header.png`);
+  await download(config.resources.footer_image, `projects/pap/src/assets/images/footer.png`);
+}
+function execCmd(cmd: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    console.log(`${cmd}: START`);
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+        console.error(stderr);
+        console.log(`${cmd}: ERROR`);
+        reject();
+      }
+      console.log(`${cmd}: DONE`);
+      resolve();
+    });
+  });
+}
+function getPaths(companyId: string): Paths {
+  const instancePath = `./instances/${companyId}`;
+  return {
+    companyId,
+    instancePath,
+    apiUrl: `${api}/api/c/${companyId}/config.json`,
+    capacitorConfigPath: `${instancePath}/capacitor-config.json`,
+    variablesConfigPath: `projects/pap/src/theme/variables.scss`,
+    devVariablesConfigPath: `projects/pap/src/theme/dev-variables.scss`,
+    environmentProdPath: `projects/pap/src/environments/environment.prod.ts`,
+    environmentDevPath: `projects/pap/src/environments/environment.ts`,
+  };
+}
 // Funzione per effettuare il login e ottenere il token di autenticazione
 async function loginAndGetAuthToken(email: string, password: string): Promise<string> {
-  const loginUrl = 'https://portapporta.webmapp.it/api/login'; // URL di login dell'API
+  const loginUrl = `${api}/api/login`; // URL di login dell'API
   const authData = {
     email,
     password,
@@ -309,8 +352,9 @@ const ionicCapSync = (): Promise<void> => {
     });
   });
 };
-const writeFile = (path: string, file: string): Promise<void> => {
+const writeFile = (path: string, file: string | null): Promise<void> => {
   return new Promise((resolve, reject) => {
+    if (file == null) reject();
     console.log(`${path}: START`);
     try {
       fs.writeFileSync(path, file);
@@ -359,7 +403,7 @@ async function readFileContent(filePath: string) {
     return data;
   } catch (err) {
     console.error('Errore durante la lettura del file:', err);
-    throw err;
+    return null;
   }
 }
 
@@ -369,6 +413,6 @@ async function download(url: string, destinationPath: string) {
     fs.writeFileSync(destinationPath, response.data);
     console.log('Il file  è stato scaricato con successo.');
   } catch (error) {
-    console.error('Si è verificato un errore durante il download del file :', error);
+    console.error('Si è verificato un errore durante il download del file :', url);
   }
 }
