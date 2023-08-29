323f4660d915f73938bb483de47c1ceea29bcd7d
bongiu <peppedeka@gmail.com>
2023-08-29 11:12:43 +0200

    Revert "build(gulp): fix scripts"
    This reverts commit 0cfbb6dbc0eb5dfb54f834489f14b5ff82e3aa30.

---
+++ capacitor.config.ts
@@ -0,0 +1,25 @@
+import {CapacitorConfig} from '@capacitor/cli';
+import {KeyboardResize, KeyboardStyle} from '@capacitor/keyboard';
+const capacitorConfig: CapacitorConfig = {
+  'appId': 'it.netseven.portapporta',
+  'appName': 'Ersu',
+  'webDir': 'dist/pap/',
+  'bundledWebRuntime': false,
+  'cordova': {
+    'preferences': {
+      'ScrollEnabled': 'false',
+      'BackupWebStorage': 'none',
+      'SplashMaintainAspectRatio': 'true',
+      'FadeSplashScreenDuration': '300',
+      'SplashShowOnlyFirstTime': 'false',
+      'SplashScreen': 'screen',
+      'SplashScreenDelay': '3000',
+    },
+  },
+  'plugins': {
+    'PushNotifications': {
+      'presentationOptions': ['badge', 'sound', 'alert'],
+    },
+  },
+};
+export default capacitorConfig;
