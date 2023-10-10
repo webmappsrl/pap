cbe57eb04561317eb51918098515e8f509fe3309
bongiu <peppedeka@gmail.com>
2023-10-10 11:50:08 +0200

    ...

--- projects/pap/src/app/shared/form/steps/second-step.component.ts
+++ projects/pap/src/app/shared/form/steps/second-step.component.ts
@@ -55,11 +55,11 @@ import {FormProvider} from '../form-provider';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class secondStepSignupComponent {
-  @Input() buttons = true;
   @Output() next: EventEmitter<void> = new EventEmitter<void>();
+  secondStep: UntypedFormGroup = this._formProvider.getForm().get('secondStep') as UntypedFormGroup;
+  @Input() buttons = true;
   @Output() prev: EventEmitter<void> = new EventEmitter<void>();
 
-  secondStep: UntypedFormGroup = this._formProvider.getForm().get('secondStep') as UntypedFormGroup;
 
   constructor(private _formProvider: FormProvider) {}
 }
