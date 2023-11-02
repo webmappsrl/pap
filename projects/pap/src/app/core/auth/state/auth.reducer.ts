f6f181fb2c2ffdc273a080fec14bb4883867bde8
peppedeka <peppedeka@gmail.com>
2023-11-02 15:33:50 +0100

    chore(auth): add logic to check if user has no address
    This commit adds logic to the auth reducer to check if the user has no address. If the user's addresses array is null or empty, the `noAddress` property will be set to true.

--- projects/pap/src/app/core/auth/state/auth.reducer.ts
+++ projects/pap/src/app/core/auth/state/auth.reducer.ts
@@ -58,6 +58,7 @@ export const reducer = createReducer(
       isLogged: true,
       user: action.user.data.user,
       isVerified: action.user.data.email_verified_at != null ? true : false,
+      noAddress: action.user.addresses == null || action.user.addresses.length === 0 ? true : false,
       error: undefined,
     };
   }),
