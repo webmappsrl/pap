b1dfd0dc5097d30b45cae60619b0fc1c09f814ed
bongiu <peppedeka@gmail.com>
2023-11-08 14:57:27 +0100

    fix: set position in map by click in my position

--- projects/pap/src/app/shared/form/location/location.component.ts
+++ projects/pap/src/app/shared/form/location/location.component.ts
@@ -1,15 +1,15 @@
 import {
-  ChangeDetectionStrategy,
   ChangeDetectorRef,
   Component,
   EventEmitter,
   Input,
+  OnDestroy,
   Output,
   ViewEncapsulation,
 } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {Store} from '@ngrx/store';
-import {BehaviorSubject, Observable} from 'rxjs';
+import {BehaviorSubject, Observable, Subscription} from 'rxjs';
 import {take} from 'rxjs/operators';
 import {AppState} from '../../../core/core.state';
 import {setMarker} from '../../map/state/map.actions';
@@ -23,39 +23,14 @@ import {AddressEvent, GeoJsonFeatureCollection, Zone} from './location.model';
   styleUrls: ['./location.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class LocationComponent {
+export class LocationComponent implements OnDestroy {
+  private _formAddressValueChangesSub: Subscription = Subscription.EMPTY;
+
   @Input() set position(coords: [number, number]) {
     if (coords != null && coords.length > 0) {
       this.myPosition$.next(coords);
       this._store.dispatch(setMarker({coords}));
     }
-  }
-  @Input() features: GeoJsonFeatureCollection[];
-  @Input() form: FormGroup;
-  @Output() addressEVT: EventEmitter<AddressEvent> = new EventEmitter<AddressEvent>();
-
-  currentZone$: Observable<Zone> = this._store.select(currentZone);
-  myPosition$: BehaviorSubject<number[]> = new BehaviorSubject<number[]>([]);
-  formAddress: FormGroup;
-  constructor(
-    private locationService: LocationService,
-    private _store: Store<AppState>,
-    private _cdr: ChangeDetectorRef,
-  ) {
-    this.formAddress = new FormGroup({
-      city: new FormControl('', Validators.required),
-      address: new FormControl('', Validators.required),
-      house_number: new FormControl('', Validators.required),
-    });
-    this.formAddress.valueChanges.subscribe(addr => {
-      console.log(addr);
-      this.addressEVT.emit({
-        location: this.myPosition$.value,
-        address: addr.address,
-        city: addr.city,
-        house_number: addr.house_number,
-      });
-    });
   }
 
   set address(addr: any) {
@@ -67,10 +42,39 @@ export class LocationComponent {
     this.setAddress(addr);
   }
 
+  @Input() features: GeoJsonFeatureCollection[];
+  @Input() form: FormGroup;
+  @Output() addressEVT: EventEmitter<AddressEvent> = new EventEmitter<AddressEvent>();
+
+  currentZone$: Observable<Zone> = this._store.select(currentZone);
+  formAddress: FormGroup;
+  myPosition$: BehaviorSubject<number[]> = new BehaviorSubject<number[]>([]);
+
+  constructor(
+    private locationService: LocationService,
+    private _store: Store<AppState>,
+    private _cdr: ChangeDetectorRef,
+  ) {
+    this.formAddress = new FormGroup({
+      city: new FormControl('', Validators.required),
+      address: new FormControl('', Validators.required),
+      house_number: new FormControl('', Validators.required),
+    });
+    this._formAddressValueChangesSub = this.formAddress.valueChanges.subscribe(addr => {
+      this.addressEVT.emit({
+        location: this.myPosition$.value,
+        address: addr.address,
+        city: addr.city,
+        house_number: addr.house_number,
+      });
+    });
+  }
+
   async getLocation(): Promise<void> {
     try {
       navigator.geolocation.getCurrentPosition(location => {
         const coords = [location.coords.longitude, location.coords.latitude] as [number, number];
+        this.setPosition(coords);
         this._store.dispatch(setMarker({coords}));
       });
     } catch (error) {
@@ -78,8 +82,12 @@ export class LocationComponent {
     }
   }
 
-  mapClick(ev: number[]): void {
-    this.setPosition(ev);
+  mapClick(coords: number[]): void {
+    this.setPosition(coords);
+  }
+
+  ngOnDestroy(): void {
+    this._formAddressValueChangesSub.unsubscribe();
   }
 
   setAddress(address: any): void {
