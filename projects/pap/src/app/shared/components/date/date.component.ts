80a89458cc47dea1cc3099ccb4b0da0e18c05cee
bongiu <peppedeka@gmail.com>
2023-11-09 15:24:52 +0100

    chore: Update date component
    - Added change detection strategy to OnPush
    - Set encapsulation to None

--- projects/pap/src/app/shared/components/date/date.component.ts
+++ projects/pap/src/app/shared/components/date/date.component.ts
@@ -1,9 +1,11 @@
-import {Component, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'pap-date',
   templateUrl: './date.component.html',
   styleUrls: ['./date.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
 export class DateComponent {
   @Input() dateKey: string;
