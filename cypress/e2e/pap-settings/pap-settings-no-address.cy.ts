813f6f89222724c666170991d5df719e8f20bc99
Rubens Garofalo <rubensgiuseppegarofalo@gmail.com>
2023-11-20 15:16:08 +0100

    ...

--- cypress/e2e/pap-settings/pap-settings-no-address.cy.ts
+++ cypress/e2e/pap-settings/pap-settings-no-address.cy.ts
@@ -6,14 +6,16 @@ before(() => {
   e2eLogin('email_no_address');
 });
 
-describe('pap-settings: test the correct behaviour of firstStep tab', () => {
+describe('pap-settings: test the correct behaviour of no address case', () => {
   it('should show an alert after login if there are no addresses', () => {
     cy.url().should('include', '/home');
     cy.get('ion-alert').should('be.visible');
   });
 
-  it('', () => {
+  it('should navigate to pap-location-modal correctly', () => {
     cy.get('.alert-button-role-ok').should('be.visible').click();
+    cy.get('pap-location-modal').should('be.visible');
+    cy.get('pap-map').should('be.visible');
   });
 });
 
