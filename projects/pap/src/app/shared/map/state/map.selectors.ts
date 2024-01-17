8ce9275fbbabaf29ee60d47a1f94c3c0ab5050e6
peppedeka <peppedeka@gmail.com>
2024-01-17 12:59:11 +0100

    fix(zones): Improve currentZone selector logic
    - Refactored the logic in the currentZone selector to handle multi-polygon geometries correctly.
    - Created a new variable "polygon" to simplify code readability.
    - Added additional checks for multi-polygon geometries and iterated through each polygon to check for intersection with coordinates.
    - Updated the return statement to prioritize single polygons, followed by multi-polygons if no single polygon is found.

--- projects/pap/src/app/shared/map/state/map.selectors.ts
+++ projects/pap/src/app/shared/map/state/map.selectors.ts
@@ -15,13 +15,24 @@ export const currentMarkerCoords = createSelector(
 export const currentZone = createSelector(confiniZone, currentMarkerCoords, (confini, coords) => {
   if (confini.length > 0 && coords.length > 0) {
     const confiniIn: any[] = confini.filter(c => {
-      const ffea = new Feature({
-        geometry: new MultiPolygon(c.geometry.coordinates),
-      });
-      return ffea.getGeometry()!.intersectsCoordinate(coords);
+      let polygon = new MultiPolygon(c.geometry.coordinates);
+      return polygon.intersectsCoordinate(coords);
+    });
+    const confiniInMulti: any[] = confini.filter(c => {
+      if (c.geometry.coordinates[0][0].length > 1) {
+        let polygonGeometries = c.geometry.coordinates[0];
+        for (const polygonGeometry of polygonGeometries) {
+          let polygon = new MultiPolygon([[polygonGeometry]]);
+          if (polygon.intersectsCoordinate(coords)) {
+            return true;
+          }
+        }
+        return false;
+      }
+      return false;
     });
 
-    return confiniIn[0] ?? undefined;
+    return confiniIn[0] ?? confiniInMulti[0] ?? undefined;
   }
   return undefined;
 });
