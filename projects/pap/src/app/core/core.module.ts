9f3dea8dbc6b8f3e0c6a1ec8931811d1b5ca89e5
Alessandro <alessandropeci@webmapp.it>
2024-07-02 16:15:29 +0200

    chore: Update core.module.ts
    - Added import for PushNotificationEffects
    - Added store module and reducer for push notification feature

--- projects/pap/src/app/core/core.module.ts
+++ projects/pap/src/app/core/core.module.ts
@@ -16,9 +16,10 @@ import {AuthInterceptor} from './auth.interceptor';
 import {reducers} from './core.state';
 import {LayoutComponent} from './layout/layout.component';
 import {LayoutEffects} from './layout/state/layout.effects';
-
 import {CalendarEffects} from '../features/calendar/state/calendar.effects';
 import {MapEffects} from '../shared/map/state/map.effects';
+import { PushNotificationEffects } from '../features/push-notification/state/push-notification.effects';
+import * as fromPushnotification from '../features/push-notification/state/push-notification.reducer'
 @NgModule({
   declarations: [LayoutComponent],
   imports: [
@@ -35,7 +36,9 @@ import {MapEffects} from '../shared/map/state/map.effects';
     StoreModule.forFeature(fromCalendar.calendarFeatureKey, fromCalendar.reducer),
     StoreModule.forFeature(fromTrashBook.trashBookFeatureKey, fromTrashBook.reducer),
     StoreModule.forFeature(fromMap.mapFeatureKey, fromMap.reducer),
-    EffectsModule.forRoot([LayoutEffects, TrashBookEffects, MapEffects, CalendarEffects]),
+    StoreModule.forFeature(fromMap.mapFeatureKey, fromMap.reducer),
+    StoreModule.forFeature(fromPushnotification.pushNotificationFeatureKey, fromPushnotification.reducer),
+    EffectsModule.forRoot([LayoutEffects, TrashBookEffects, MapEffects, CalendarEffects, PushNotificationEffects]),
     IonicModule.forRoot({
       rippleEffect: false,
       mode: 'md',
