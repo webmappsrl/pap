445f2acb0ff7709cc676d25bef60f181aba2bab8
Alessandro <alessandropeci@webmapp.it>
2024-07-08 12:56:48 +0200

    feat(push-notification): improve push notification page layout and functionality
    - Updated HTML template to display push notifications in an accordion format.
    - Added card header with title "Le mie notifiche".
    - Added date and time information for each push notification.
    - Updated SCSS file to style the new elements.
    - Added logic to set the value of the firstNotificationId$ BehaviorSubject based on the first notification in the list.
    - Dispatched removeAllDeliveredNotifications action on component initialization.

--- projects/pap/src/app/features/push-notification/push-notification-page.component.ts
+++ projects/pap/src/app/features/push-notification/push-notification-page.component.ts
@@ -1,8 +1,10 @@
-import { Component, ViewEncapsulation } from "@angular/core";
-import { Observable } from "rxjs";
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { BehaviorSubject, Observable } from "rxjs";
 import { PushNotification } from "./push-notification.model";
 import { Store } from "@ngrx/store";
 import { pushNotifications } from "./state/push-notification.selectors";
+import { take } from "rxjs/operators";
+import { removeAllDeliveredNotifications } from "./state/push-notification.actions";
 
 @Component({
   selector: 'pap-push-notification-page',
@@ -11,8 +13,20 @@ import { pushNotifications } from "./state/push-notification.selectors";
   encapsulation: ViewEncapsulation.None,
 })
 
-export class PushNotificationsPageComponent {
+export class PushNotificationsPageComponent implements OnInit{
+  firstNotificationId$: BehaviorSubject<number | undefined> = new BehaviorSubject<number | undefined>(undefined);
   pushNotifications$: Observable<PushNotification[] | undefined> = this._store.select(pushNotifications);
 
   constructor(private _store: Store){}
+
+  ngOnInit(): void {
+    this._store.dispatch(removeAllDeliveredNotifications());
+    this.pushNotifications$
+    .pipe(take(1))
+    .subscribe((notifications: PushNotification[] | undefined) => {
+      if (notifications && notifications.length > 0) {
+        this.firstNotificationId$.next(notifications[0].id);
+      }
+    });
+  }
 }
