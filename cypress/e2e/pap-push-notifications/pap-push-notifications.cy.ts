5a2b3606a5cb692c864ec609f44ffa6c5c98f99d
peco11 <peco11@users.noreply.github.com>
2024-07-15 09:46:09 +0000

    style: format code

--- cypress/e2e/pap-push-notifications/pap-push-notifications.cy.ts
+++ cypress/e2e/pap-push-notifications/pap-push-notifications.cy.ts
@@ -1,4 +1,4 @@
-import { clearTestState, e2eLogin } from "cypress/utils/test-utils";
+import {clearTestState, e2eLogin} from 'cypress/utils/test-utils';
 import {environment} from 'projects/pap/src/environments/environment';
 
 const apiPushNotifications = `${environment.api}/c/${environment.companyId}/pushnotification`;
@@ -6,14 +6,14 @@ const apiPushNotifications = `${environment.api}/c/${environment.companyId}/push
 before(() => {
   clearTestState();
   cy.visit(Cypress.env('baseurl'));
-})
+});
 
 describe('pap-push-notifications: test correct behavior when user is not logged in', () => {
   it('should not show notifications button if not logged in', () => {
     cy.visit(Cypress.env('baseurl'));
     cy.get('.pap-header-button-notifications').should('not.exist');
   });
-})
+});
 
 describe('pap-push-notifications: test correct behavior when user is logged in', () => {
   it('should show notifications button if logged in', () => {
@@ -31,7 +31,7 @@ describe('pap-push-notifications: test correct behavior when user is logged in',
   it('should display the notifications correctly', () => {
     cy.get('.notification-content').should('have.length.greaterThan', 0);
   });
-})
+});
 
 describe('pap-push-notifications: Test the correct behavior when I have no notifications', () => {
   before(() => {
@@ -42,14 +42,14 @@ describe('pap-push-notifications: Test the correct behavior when I have no notif
   it('should show "Non ci sono notifiche da visualizzare" when there are no notifications', () => {
     e2eLogin().then(() => {
       cy.intercept('GET', apiPushNotifications, {
-        body: []
+        body: [],
       }).as('getPushNotificationsEmpty');
       cy.get('.pap-header-button-notifications').click();
       cy.wait('@getPushNotificationsEmpty');
       cy.get('p').contains('Non ci sono notifiche da visualizzare').should('be.visible');
     });
   });
-})
+});
 
 after(() => {
   clearTestState();
