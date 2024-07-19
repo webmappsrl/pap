fe06331d71336a372910e3aa1f59bbde4b019fe1
Alessandro <alessandropeci@webmapp.it>
2024-07-19 14:58:37 +0200

    feat: add selectRequiredFields selector
    This commit adds a new selector `selectRequiredFields` to the `form-fields.selectors.ts` file. This selector filters the form fields based on certain criteria, including excluding the 'password' and 'password_confirmation' fields.

--- projects/pap/src/app/shared/form/state/form-fields.selectors.ts
+++ projects/pap/src/app/shared/form/state/form-fields.selectors.ts
@@ -8,5 +8,13 @@ export const selectFormJsonByStep = (step: number) => createSelector(
   selectFormJsonState,
   state => state.formJson?.filter(item => item.step === step)
 );
+export const selectRequiredFields = createSelector(
+  selectFormJsonState,
+  state => state.formJson?.filter(field => (
+    field.validators?.some(validator => validator.name === 'required') &&
+    field.id !== 'password' &&
+    field.id !== 'password_confirmation'
+  ))
+);
 export const selectError = createSelector(selectFormJsonState, state => state.error);
 export const selectLoading = createSelector(selectFormJsonState, state => state.loading);
