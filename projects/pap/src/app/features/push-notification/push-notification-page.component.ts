9c1f46aaa1e69728a8604f854cff7c002fd5944d
peco11 <alessandropeci@webmapp.it>
2024-12-23 09:35:24 +0100

    chore: Update BroadcastNotificationService oc: 4509 (#71)
    * chore: Update BroadcastNotificationService
    
    - Changed the dispatch action to navigate to '/push-notification' instead of loading push notification
    - Added a condition to navigate to reports page only if ticket_id is present
    - Updated the _navigateToReportsPage method to accept a non-null ticketId parameter and filter reports based on the selected ticket
    
    chore: add firstNotificationId to PushNotificationState
    
    - Added the property `firstNotificationId` to the `PushNotificationState` interface.
    - Updated the reducer to set `firstNotificationId` as the id of the first push notification in the state.
    - Added a new selector `firstNotificationId` to retrieve the value of `firstNotificationId` from the state.
    
    chore: Update push-notification-page.component.ts
    
    - Import ChangeDetectorRef and NgZone from '@angular/core'
    - Import Subscription from 'rxjs'
    - Update the imports for pushNotifications and removeAllDeliveredNotifications
    - Add private variables for _firstNotificationIdSub and _pushNotificationsSub
    - Update the type of firstNotificationId$ to Observable<number | undefined>
    - Update the type of pushNotifications$ to Observable<PushNotification[] | undefined>
    - Add ngOnDestroy method to unsubscribe from subscriptions
    - Dispatch removeAllDeliveredNotifications action in ngOnInit
    - Dispatch loadPushNotification action in ngOnInit
    - Subscribe to pushNotifications$ and call detectChanges in _ngZone.run()
    - Subscribe to firstNotificationId$ and call detectChanges in _ngZone.run()
    
    * style: format code
    
    * chore: Update API version in environment.ts file
    
    The API URL in the environment.ts file has been updated from v1 to v2.
    
    * chore: Update styles.scss to include new alert button role for delete confirmation
    
    * chore: Remove unused code and console.log statements
    
    - Removed unused private variable `_pushNotificationsSub` in `PushNotificationsPageComponent`
    - Removed console.log statement in `_navigateToReportsPage` method of `BroadcastNotificationService`
    
    ---------
    
    Co-authored-by: peco11 <peco11@users.noreply.github.com>

--- projects/pap/src/app/features/push-notification/push-notification-page.component.ts
+++ projects/pap/src/app/features/push-notification/push-notification-page.component.ts
@@ -1,10 +1,12 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {Observable} from 'rxjs';
+import {ChangeDetectorRef, Component, NgZone, OnInit, ViewEncapsulation} from '@angular/core';
+import {Observable, Subscription} from 'rxjs';
 import {PushNotification} from './push-notification.model';
 import {Store} from '@ngrx/store';
-import {pushNotifications} from './state/push-notification.selectors';
-import {filter, map, take} from 'rxjs/operators';
-import {removeAllDeliveredNotifications} from './state/push-notification.actions';
+import {firstNotificationId, pushNotifications} from './state/push-notification.selectors';
+import {
+  loadPushNotification,
+  removeAllDeliveredNotifications,
+} from './state/push-notification.actions';
 
 @Component({
   selector: 'pap-push-notification-page',
@@ -13,18 +15,27 @@ import {removeAllDeliveredNotifications} from './state/push-notification.actions
   encapsulation: ViewEncapsulation.None,
 })
 export class PushNotificationsPageComponent implements OnInit {
-  firstNotificationId$: Observable<number>;
+  private _firstNotificationIdSub: Subscription = Subscription.EMPTY;
+
+  firstNotificationId$: Observable<number | undefined> = this._store.select(firstNotificationId);
   pushNotifications$: Observable<PushNotification[] | undefined> =
     this._store.select(pushNotifications);
 
-  constructor(private _store: Store) {}
+  constructor(
+    private _store: Store,
+    private _cdr: ChangeDetectorRef,
+    private _ngZone: NgZone,
+  ) {}
+
+  ngOnDestroy(): void {
+    this._firstNotificationIdSub.unsubscribe();
+  }
 
   ngOnInit(): void {
     this._store.dispatch(removeAllDeliveredNotifications());
-    this.firstNotificationId$ = this.pushNotifications$.pipe(
-      filter(notifications => !!notifications && notifications.length > 0),
-      take(1),
-      map(notifications => notifications![0].id),
+    this._store.dispatch(loadPushNotification());
+    this._firstNotificationIdSub = this.firstNotificationId$.subscribe(() =>
+      this._ngZone.run(() => this._cdr.detectChanges()),
     );
   }
 }
